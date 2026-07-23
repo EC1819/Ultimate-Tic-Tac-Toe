ba4413682a66c005102b61ed8a6e529967c96670
Manny <emmanuelcanizales@gmail.com>
2026-07-22 22:23:38 -0700

    Added in comments explaining code

--- Ultimate Logic/src/Main.java
+++ Ultimate Logic/src/Main.java
@@ -5,19 +5,24 @@ public class Main {
         //Creating a fixed game board size
         char[][] gameBoard = new char[11][11];
 
+        //Setting all the spots on the board to be empty
         for(int row = 0; row < gameBoard.length; row++) {
             for(int col = 0; col < gameBoard[row].length; col++) {
-                gameBoard[row][col] = 'X';
+                gameBoard[row][col] = ' ';
             }
         }
+        //Setting the boarders for the other cells
+        //Changing the empty spaces in columns 3 and 7 to have a line
         for(int row = 0; row < gameBoard.length; row++) {
             gameBoard[row][3] = '|';
             gameBoard[row][7] = '|';
         }
+        //Changing the empty spaces in rows 3 and 7 to have line
         for(int col = 0; col < gameBoard.length; col++){
             gameBoard[3][col] = '-';
             gameBoard[7][col] = '-';
         }
+        //Changing specific spots to have a plus sign to match board set up
         gameBoard[3][3] = '+';
         gameBoard[3][7] = '+';
         gameBoard[7][3] = '+';
@@ -27,6 +32,7 @@ public class Main {
 
     }
 
+    //Helper method to print out the game board
     public static void printingGameBoard(char[][] gameBoard) {
         for(char[] row : gameBoard) {
             for(char col : row) {
